55df12d8dffebe532a92fe1e804ef3bb87587a07
Chinami Kitagawa <cnm25.k@gmail.com>
2020-08-01 20:21:32 +0900

    [finish]5-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '73289d40827008c8d8fff6a1aa26ff1cad4d2b863c3dfadd940dd9b6aefc34a9f307bb9cf1ea4976b49e0ede73352c9507ae6710c64655a82c345013a7adaa2c'
